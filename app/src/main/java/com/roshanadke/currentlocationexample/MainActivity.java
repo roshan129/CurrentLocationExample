44135bead20230e312b6256187507127d7905482
microsoft <roshanadke2015@gmail.com>
2020-01-13 10:33:18 +0530

    Initial commit

---
+++ app/src/main/java/com/roshanadke/currentlocationexample/MainActivity.java
@@ -0,0 +1,78 @@
+package com.roshanadke.currentlocationexample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.FileObserver;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView txt;
+    private FusedLocationProviderClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        txt = findViewById(R.id.textView);
+        requestPermission();
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+
+                client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                        if (location != null) {
+                            txt.setText(location.toString());
+                            Log.d("tag", "onSuccess: "+ location.getLatitude());
+                            Log.d("tag", "onSuccess: "+ location.getLongitude());
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            Toast.makeText(this, "clciked", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
